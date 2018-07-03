2bdf4fa601dc75b5f18e3161c96f0c01a0da7a51
xuwei <xuwei@happytransfer.cn>
2018-07-04 01:41:49 +0800

    as

--- pages/newsDetail/index.js
+++ pages/newsDetail/index.js
@@ -23,14 +23,12 @@ Page({
       method: "GET",
       success: function (res) {
         var article = res.data.data.content;
-        console.log(res.data.data);
         var _data = res.data.data;
         _data.lasttime = getApp().formartData(_data.lasttime)
         self.setData({
           detail: _data
         });
         wx.setNavigationBarTitle({ title: res.data.data.title});
-
         WxParse.wxParse('article', 'html', article, self, 5);
         wx.hideLoading()
       }
